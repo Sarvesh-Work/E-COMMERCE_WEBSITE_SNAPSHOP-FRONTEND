892c371729722da09fbc894bb064b5735ce012d9
Sarvesh-Work <sarveshmunde10@gmail.com>
2024-06-19 20:36:24 +0530

    --

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsxx'
+import App from './App.js'
 import { Provider } from 'react-redux'
 import { store } from './app/store';
 
